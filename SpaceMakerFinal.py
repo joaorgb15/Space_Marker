52ebf558c6df1b3e13d9d9932597517e368e2719
joaorgb15 <137447710+joaorgb15@users.noreply.github.com>
2023-06-26 19:22:14 -0300

    Comecei a fazer os def

--- SpaceMakerFinal.py
+++ SpaceMakerFinal.py
@@ -18,3 +18,20 @@ pygame.mixer.music.play(-1)
 
 white=(255, 255, 255)
 black=(0, 0, 0)
+
+def mouse_over_line(mouse_position, pos_start, pos_end):
+    limit = 5
+    x, y = mouse_position
+    x1, y1 = pos_start
+    x2, y2 = pos_end
+
+    denominator=((y2 -y1) ** 2 + (x2 -x1) **2)** 0.5
+    if denominator !=0:
+        distance=abs((y2 - y1) * x - (x2 -x1) * y + x2 * y1 -y2 * x1)/denominator
+    else:
+        distance=0
+    return distance <= limit
+
+def display_text(txt, font, color, x, y):
+    text_surface = font.render(txt, True, color)
+    display_game.blit(text_surface, (x, y))
